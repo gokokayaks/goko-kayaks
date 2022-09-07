efe3a4d9af2232762e2874839b62c546efcb7dd7
Ben Archer <ben.archer@hotmail.co.uk>
2022-09-07 11:55:45 +0100

    added canonical url

--- webpack.config.js
+++ webpack.config.js
@@ -21,6 +21,7 @@ module.exports = ({ NODE_ENV = 'production' } = {}) => {
           title: 'Goko Kayaks',
           description:
             'Goko Kayaks is a quality kayak tour and rental service. We are a mobile business bringing rentals to you throughout the Tampa Bay Area. We offer an unforgettable Eco-Tour launching right off Gandy Beach, St.Petersburg, and into a whole other Florida!We also offer a Sunset Tour near Shell Key, Paddlesport Instruction Classes, Team-building & Parties. Goko prides itself on values of diversity, sustainability and great customer service. Experience nature & wildlife in the Tampa Bay area. Birds, dolphin, manatee, mangroves and more! Endless summer and adventure. Located in St Petersburg."><meta name="keywords" content="Kayaking st petersburg,Kayaking st pete,Kayaking Clearwater,Kayaking Tampa Bay,Paddleboarding st Pete,paddleboarding Clearwater,paddleboarding Tampa Bay,Best kayaking near me,Best Eco-tour,mangrove kayaking,kayaking Reddington beach,kayaking madeira beach,kayaking Shell Key Preserve,kayaking weedon island,kayaking honeymoon island, kayaking st,pete,kayaking gandy beach,Paddleboard rental,kayak rental,Sunset tour,fun things to do st.pete,kayaking st. Petersburg,team Building,SUP lessons,kayaking lessons,see dolphins,see manatee',
+          canonical: 'https://www.gokokayaks.com',
           template: './build/template.html',
         }),
         ...envConfig.plugins,
