0ad95ad4fbf96fa2c8107ee89df8b3e21a7c6e63
Ben Archer <ben.archer@hotmail.co.uk>
2023-08-21 23:19:17 +0100

    add 2023 award image

--- src/pages/home/sections/award.tsx
+++ src/pages/home/sections/award.tsx
@@ -2,8 +2,10 @@ import React from 'react';
 import { Section } from '../../../components/section';
 import { Text } from '../../../components/text';
 import { Image } from '../../../components/image';
-import bestOfTheBestWebp from '../../../images/best-of-the-best-2022.webp';
-import bestOfTheBestPng from '../../../images/best-of-the-best-2022.png';
+import bestOfTheBestWebp2022 from '../../../images/best-of-the-best-2022.webp';
+import bestOfTheBestPng2022 from '../../../images/best-of-the-best-2022.png';
+import bestOfTheBestWebp2023 from '../../../images/best-of-the-best-2023.webp';
+import bestOfTheBestPng2023 from '../../../images/best-of-the-best-2023.png';
 import styled from 'styled-components';
 import { device } from '../../../common/styles/breakpoints';
 
@@ -18,9 +20,10 @@ const ImageWrapper = styled(Image)`
 export const AwardSection = () => {
   return (
     <Section color="blue" alignment="center">
-      <ImageWrapper webp={bestOfTheBestWebp} fallback={bestOfTheBestPng} alt="Best of the best award winners" />
+      <ImageWrapper webp={bestOfTheBestWebp2022} fallback={bestOfTheBestPng2022} alt="Best of the best award winners 2022" />
+      <ImageWrapper webp={bestOfTheBestWebp2023} fallback={bestOfTheBestPng2023} alt="Best of the best award winners 2023" />
       <Text type="subHeading" color="white" alignment="center">
-        Peoples Choice 2022 Gold Winners
+        Peoples Choice 2022 and 2023 Gold Winners
       </Text>
       <Text type="paragraph" color="white" alignment="center">
         Voted Best Kayak Rental Company in Tampa Bay
